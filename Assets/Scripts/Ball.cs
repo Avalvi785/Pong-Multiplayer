92e74b2fb45decc3ba5918d9d81a7ee1078fe709
Avalvi785 <134474692+Avalvi785@users.noreply.github.com>
2025-09-01 07:21:12 +0530

    Update ball movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,40 +1,32 @@
 ﻿using Fusion;
-using Fusion.Addons.Physics;
 using UnityEngine;
 
 public class Ball : NetworkBehaviour
 {
-    [Networked] private Vector2 Direction { get; set; }
     [SerializeField] private float speed = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     public override void Spawned()
     {
         if (Object.HasStateAuthority) // Only server moves the ball
         {
-            // Start with random direction
-            Direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-0.5f, 0.5f)).normalized;
+            Vector2 dir = new Vector2(
+            Random.value < 0.5f ? -1 : 1,
+            Random.Range(-0.5f, 0.5f)
+        ).normalized;
+
+            rb.linearVelocity = dir * speed;
         }
-    }
-
-    public override void FixedUpdateNetwork()
-    {
-        if (Object.HasStateAuthority) // Only server moves the ball
-        {
-            transform.position += (Vector3)(Direction * speed * Runner.DeltaTime);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (!Object.HasStateAuthority) return;
-
-        Vector2 normal = collision.contacts[0].normal;
-        Direction = Vector2.Reflect(Direction, normal).normalized;
-        Debug.LogError("Ball collided with " + collision.gameObject.name);
     }
 
     public void OnNetworkDespawn()
     {
-       Runner.Despawn(Object);
+        Runner.Despawn(Object);
     }
 }
